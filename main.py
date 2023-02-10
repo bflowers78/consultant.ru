6e707f63baaf0640823954bfacbdbb91e60cf221
Vyacheslav Osipov <gazvpalace@gmail.com>
2023-02-10 23:37:05 +0500

    -небольшие изменения

--- main.py
+++ main.py
@@ -9,53 +9,75 @@ def cooking(url):
     response.encoding = 'utf-8'
     return BeautifulSoup(response.text, 'lxml')
 
-exeption = ['/document/cons_doc_LAW_28399/820b4ecd6532a57dd0ff9871cd58c7e0e4542eec/', '/document/cons_doc_LAW_37800/1491c55a429ae57a6cfb2a4075fb018e65a854f6/']
-exeption_2 = ['/document/cons_doc_LAW_37800/a6eea06e5adf5cff155294be9f00362a1cd80361/']
+
+exeption = [
+    '/document/cons_doc_LAW_371232/',
+    '/document/cons_doc_LAW_315739/',
+    '/document/cons_doc_LAW_32451/94b17bafb57578dce2b04c8941777f7b2b7922cb/']
 soup = cooking('https://www.consultant.ru/popular/')
 time.sleep(2)
-rules = [x.text for x in soup.find('div', id='content').find_all('h3')]
-blockquotes = soup.find_all('blockquote')[:-1]
+rules = [x.text for x in soup.find('div', id='content').find_all('h3')][1]
+blockquotes = soup.find_all('blockquote')[:-1][1]
 result = {}
-for name_1, block in zip(rules, blockquotes):
-    print(name_1)
+'''for name_1, block in zip(rules, blockquotes):
+    print(name_1, '  ***')
     result[name_1] = {}
-    links = block.find_all('a')
-    for link in links:
-        name_2 = link.text
-        result[name_1][link.text] = {}
-        soup = cooking(f'https://www.consultant.ru/{link["href"]}')
-        sect = soup.find('div', class_='document-page__toc').find('li')
-        while sect:
-            if sect.name == 'li': #  Разделы
-                name_3 = sect.text
-                result[name_1][name_2][name_3] = {}
-                print(name_3)
-                url = sect.find('a')['href']
-                soup = cooking(f"https://www.consultant.ru/{url}")
-                if url in exeption:
-                    result[name_1][name_2][name_3] = soup.find('div', attrs={'class': 'document-page__content document-page_left-padding'}).text
-                else:
-                    chapters = soup.find('div', class_='document-page__toc').find_all('li')
-                    for chap in chapters:
-                        name_4 = chap.text
-                        result[name_1][name_2][name_3][name_4] = {}
-                        print(name_4)
-                        chap_link = chap.find('a')['href']
-                        soup = cooking(f"https://www.consultant.ru/{chap_link}")
-                        if chap_link in exeption or url in exeption_2:
-                            result[name_1][name_2][name_3][name_4] = soup.find('div', attrs={'class': 'document-page__content document-page_left-padding'}).text
-                        else:
-                            articles = soup.find('div', class_='document-page__toc').find_all('li')
-                            for article in articles:
-                                name_5 = article.find('a').text
-                                result[name_1][name_2][name_3][name_4][name_5] = {}
-                                link = article.find('a')['href']
-                                print(name_5)
-                                soup = cooking(f"https://www.consultant.ru/{link}")
-                                text = soup.find('div', class_='document-page__content document-page_left-padding').text
-                                result[name_1][name_2][name_3][name_4][name_5] = text
-            sect = sect.next_sibling
-with open('rules.json', 'w', encoding='utf-8') as file:
-    json.dump(result, file, indent=4, ensure_ascii=False)
-    print('Готово!')
+    links = block.find_all('a')'''
+name_1 = rules
+print(name_1, '  ***')
+result[name_1] = {}
+links = blockquotes.find_all('a')
+for link in [links[13], links[14]]:
+    if link in exeption:
+        continue
+    name_2 = link.text
+    print(name_2)
+    result[name_1][link.text] = {}
+    soup = cooking(f'https://www.consultant.ru/{link["href"]}')
+    sect = soup.find('div', class_='document-page__toc').find_all('li')
+    print(sect)
+    while sect:
+        if sect.name == 'li':
+            name_3 = sect.text
+            print(name_3, '------------------------------------------------------')
+            result[name_1][name_2][name_3] = {}
+            try:
+                link_2 = sect.find('a')['href']
+                soup = cooking(f"https://www.consultant.ru/{link_2}")
+                chapters = soup.find('div', class_='document-page__toc').find_all('li')
+                for chap in chapters:
+                    name_4 = chap.text
+                    print(name_4)
+                    result[name_1][name_2][name_3][name_4] = {}
+                    try:
+                        link_3 = chap.find('a')['href']
+                        soup = cooking(f"https://www.consultant.ru/{link_3}")
+                        articles = soup.find('div', class_='document-page__toc').find_all('li')
+                        for article in articles:
+                            name_5 = article.find('a').text
+                            print(name_5)
+                            result[name_1][name_2][name_3][name_4][name_5] = {}
+                            try:
+                                link_4 = article.find('a')['href']
+                                soup = cooking(f"https://www.consultant.ru/{link_4}")
+                                articles_2 = soup.find('div', class_='document-page__toc').find_all('li')
+                                for article_2 in articles_2:
+                                    name_6 = article_2.find('a').text
+                                    print(name_6)
+                                    result[name_1][name_2][name_3][name_4][name_5][name_6] = {}
+                                    soup = cooking(f"https://www.consultant.ru/{article_2.find('a')['href']}")
+                                    result[name_1][name_2][name_3][name_4][name_5][name_6] = soup.find('div', class_='document-page__content document-page_left-padding').text.split('\n\n')[-2]
+                            except Exception as err:
+                                print(err)
+                                result[name_1][name_2][name_3][name_4][name_5] = soup.find('div', class_='document-page__content document-page_left-padding').text.split('\n\n')[-2]
+                    except Exception as err:
+                        print(err)
+                        result[name_1][name_2][name_3][name_4] = soup.find('div', class_='document-page__content document-page_left-padding').text.split('\n\n')[-2]
+            except Exception as err:
+                print(err)
+                result[name_1][name_2][name_3] = soup.find('div', class_='document-page__content document-page_left-padding').text.split('\n\n')[-2]
+        sect = sect.next_sibling
+    with open('rules.json', 'w', encoding='utf-8') as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
+        print('Готово!')
 
